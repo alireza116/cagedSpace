8ff891d6ac6d4b3252c6382f1038d785bee300ef
UNCCHARLOTTE-NT\akarduni <akarduni@uncc.edu>
2018-08-18 12:25:32 -0400

    fixed coloring

--- app.js
+++ app.js
@@ -34,24 +34,24 @@ var messages = [];
 // });
 var countUsers = 0;
 {locations: '-80.85, 35.1932, -80.80, 35.2541'}
-// tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'}, function(stream) {
-//     stream.on('data', function(event) {
-//         var message = {}
-//         console.log(event.text);
-//         if ("media" in event.entities){
-//                 message["image"] = event.entities.media[0]["media_url"]
+tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'}, function(stream) {
+    stream.on('data', function(event) {
+        var message = {}
+        console.log(event.text);
+        if ("media" in event.entities){
+                message["image"] = event.entities.media[0]["media_url"]
             
-//         }
-//         message["text"] = event.text
-//         // console.log(message);
-//         io.emit("messages",message);
-//     //   console.log(event && event.text);
-//     });
+        }
+        message["text"] = event.text
+        // console.log(message);
+        io.emit("messages",message);
+    //   console.log(event && event.text);
+    });
    
-//     stream.on('error', function(error) {
-//       console.log( error);
-//     });
-//   });
+    stream.on('error', function(error) {
+      console.log( error);
+    });
+  });
 // var messages = [];
 // for (var i =0; i < 100; i++){
 //     messages.push("kir");
@@ -65,43 +65,42 @@ io.on('connection', function(client) {
     console.log('Client connected...');
     c = client;
     client.on('join', function(data) {
-        countUsers ++
-        console.log(countUsers + " users connected");
-        // console.log(data);
-        // messages.forEach(function(m){
-        //     client.emit("messages",m);
-        // })
-            if (countUsers ==1){
-                stream = tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'});
-                stream.on('data', function(event) {
-                    var message = {}
-                    // console.log(event.text);
-                    if ("media" in event.entities){
-                            message["image"] = event.entities.media[0]["media_url"]
+    //     countUsers ++
+    //     console.log(countUsers + " users connected");
+    //     // console.log(data);
+    //     // messages.forEach(function(m){
+    //     //     client.emit("messages",m);
+    //     // })
+    //         if (countUsers ==1){
+    //             stream = tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'});
+    //             stream.on('data', function(event) {
+    //                 var message = {}
+    //                 // console.log(event.text);
+    //                 if ("media" in event.entities){
+    //                         message["image"] = event.entities.media[0]["media_url"]
                         
-                    }
-                    message["text"] = event.text
-                    // console.log(message);
-                    io.emit("messages",message);
-                        // console.log(event && event.text);
-                    });
+    //                 }
+    //                 message["text"] = event.text
+    //                 // console.log(message);
+    //                 io.emit("messages",message);
+    //                     // console.log(event && event.text);
+    //                 });
             
-                    stream.on('error', function(error) {
-                        console.log( error);
-                     });
-            }
-            // console.log(io.sockets.clients());
+    //                 stream.on('error', function(error) {
+    //                     console.log( error);
+    //                  });
+    //         }
+    //         // console.log(io.sockets.clients());
 
-
-    client.on("leave",function(data){
-        countUsers--;
-        console.log(countUsers + " users connected");
-        if (countUsers == 0) {
-            stream.destroy();
-        }
+    // client.on("leave",function(data){
+    //     countUsers--;
+    //     console.log(countUsers + " users connected");
+    //     if (countUsers == 0) {
+    //         // stream.destroy();
+    //     }
         
-        console.log("no stream")
-    })
+    //     console.log("no stream")
+    // })
     })});
 
 io.on("disconnect",function(d){
