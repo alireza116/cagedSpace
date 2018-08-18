0041bc6161acd7be5982276609ef29955962a8d6
UNCCHARLOTTE-NT\akarduni <akarduni@uncc.edu>
2018-08-17 23:13:10 -0400

    tiwtter stream close and open

--- app.js
+++ app.js
@@ -32,29 +32,79 @@ var messages = [];
 //     // res.send(req);
     
 // });
-
-client.stream('statuses/filter', {location: '-80.85, 35.1932, -80.80, 35.2541'}, function(stream) {
-    stream.on('data', function(event) {
-        io.emit("messages",event.text);
-      console.log(event && event.text);
-    });
+var countUsers = 0;
+{locations: '-80.85, 35.1932, -80.80, 35.2541'}
+// tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'}, function(stream) {
+//     stream.on('data', function(event) {
+//         var message = {}
+//         console.log(event.text);
+//         if ("media" in event.entities){
+//                 message["image"] = event.entities.media[0]["media_url"]
+            
+//         }
+//         message["text"] = event.text
+//         // console.log(message);
+//         io.emit("messages",message);
+//     //   console.log(event && event.text);
+//     });
    
-    stream.on('error', function(error) {
-      throw error;
-    });
-  });
+//     stream.on('error', function(error) {
+//       console.log( error);
+//     });
+//   });
+// var messages = [];
+// for (var i =0; i < 100; i++){
+//     messages.push("kir");
+// }
+
+var stream; 
+
+
 
 io.on('connection', function(client) {  
     console.log('Client connected...');
     c = client;
     client.on('join', function(data) {
+        countUsers ++
         // console.log(data);
         // messages.forEach(function(m){
         //     client.emit("messages",m);
         // })
-        client.emit("messages","Welcome!");
-        // client.emit('messages', 'Hello from server');
+            if (countUsers ==1){
+                stream = tweetClient.stream('statuses/filter', {locations: '-80.913815,35.188787,-80.76756,35.274179'});
+
+            }
+            // console.log(io.sockets.clients());
+
+            stream.on('data', function(event) {
+            var message = {}
+            // console.log(event.text);
+            if ("media" in event.entities){
+                    message["image"] = event.entities.media[0]["media_url"]
+                
+            }
+            message["text"] = event.text
+            // console.log(message);
+            io.emit("messages",message);
+                console.log(event && event.text);
+            });
+    
+            stream.on('error', function(error) {
+                console.log( error);
+             });
+    client.on("leave",function(data){
+        countUsers--;
+        if (countUsers == 0) {
+            stream.destroy();
+        }
+        
+        console.log("no stream")
+    })
     })});
+
+io.on("disconnect",function(d){
+    console.log("disconnected");
+})
 
 server.listen(port);
 console.log('api running on ' + port);
